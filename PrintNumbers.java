ebfa0d92bc4d8a3ead2955521dd215d6d567f1ba
Nikhil Nigam <nikhilnigam2001@gmail.com>
2026-04-01 22:57:09 +0530

    Add files via upload

---
+++ PrintNumbers.java
@@ -0,0 +1,12 @@
+
+public class PrintNumbers {
+   public PrintNumbers() {
+   }
+
+   public static void main(String[] var0) {
+      for(int var1 = 1; var1 <= 10; ++var1) {
+         System.out.println(var1);
+      }
+
+   }
+}
